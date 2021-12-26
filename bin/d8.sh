e9331c0452d614d85a53b412785d56e3efb6e604
David Buchanan <d@vidbuchanan.co.uk>
2021-12-26 04:55:07 +0000

    Add tooling bins

---
+++ bin/d8.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+BASEDIR=$(dirname "$0")
+qemu-arm $BASEDIR/d8_libs/ld-2.24.so --library-path $BASEDIR/d8_libs/ $BASEDIR/d8
---
+++ bin/mksnapshot.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+BASEDIR=$(dirname "$0")
+qemu-i386 $BASEDIR/mksnapshot_libs/ld-2.31.so --library-path $BASEDIR/mksnapshot_libs/ $BASEDIR/mksnapshot
