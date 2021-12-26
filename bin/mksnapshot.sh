a127ea3a1ab5841dfe63a1985540c6cddf6c6618
David Buchanan <d@vidbuchanan.co.uk>
2021-12-26 04:57:11 +0000

    Enable passing args to bin runner

--- bin/mksnapshot.sh
+++ bin/mksnapshot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 BASEDIR=$(dirname "$0")
-qemu-i386 $BASEDIR/mksnapshot_libs/ld-2.31.so --library-path $BASEDIR/mksnapshot_libs/ $BASEDIR/mksnapshot
+qemu-i386 $BASEDIR/mksnapshot_libs/ld-2.31.so --library-path $BASEDIR/mksnapshot_libs/ $BASEDIR/mksnapshot $@
